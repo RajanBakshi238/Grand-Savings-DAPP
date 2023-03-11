6bf582cbd6b25335d5a3596199d33bfc4f1eaa89
RajanBakshi238 <rajan.bakshi238@gmail.com>
2023-03-12 00:03:44 +0530

    deploy script done

--- grand_savings_hardhat/deploy/deploy.js
+++ grand_savings_hardhat/deploy/deploy.js
@@ -1,14 +1,19 @@
 const { network } = require("hardhat");
 
-module.exports = async ({getNamedAccounts, deployments}) => {
-    const {deploy, log} = deployments;
-    const { deployer } = await getNamedAccounts();
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
 
-    log(network, "Deploying contract waiting for confirmations.....", network.config.blockConfirmations);
-    // const fundMe = await  deploy("CryptoKids", {
-    //     from: deployer,
-    //     args: [],
-    //     log: true,
-    //     waitConfirmations: network
-    // })
-}
+  // log(network, "Deploying contract waiting for confirmations.....", network.config.blockConfirmations);
+  log("Deploying contract waiting for confirmations.....");
+
+  const fundMe = await deploy("CryptoKids", {
+    from: deployer,
+    args: [],
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
+  });
+
+  log("Fund Me Address : ", fundMe.address, network.name);
+
+};
--- grand_savings_hardhat/hardhat.config.js
+++ grand_savings_hardhat/hardhat.config.js
@@ -1,22 +1,25 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("hardhat-deploy");
 
-const GOERLI_RPC_URL = "https://eth-goerli.g.alchemy.com/v2/OMIqacr1nhkHZCP3LZmyTh12dAQqy0D5";
-const PRIVATE_KEY = "49759754577c8f7559b2e3b72c833f457add16095da100f123f93b6a7f3bc048";
-const ETHERSCAN_API_KEY = "GTDEV7ZK82P21CG29M4EZYTAXBP63GIKC7"
-
-
+const GOERLI_RPC_URL =
+  "https://eth-goerli.g.alchemy.com/v2/OMIqacr1nhkHZCP3LZmyTh12dAQqy0D5";
+const PRIVATE_KEY =
+  "49759754577c8f7559b2e3b72c833f457add16095da100f123f93b6a7f3bc048";
+const ETHERSCAN_API_KEY = "GTDEV7ZK82P21CG29M4EZYTAXBP63GIKC7";
 
 module.exports = {
   solidity: "0.8.7",
   defaultNetwork: "hardhat",
   networks: {
-    hardhat:{
-      blockConfirmations: 10   // just for seeing config is being available in deploy script or not js 
-    }
-    // goerli: {
-
-    // }
+    hardhat: {
+      blockConfirmations: 1, // just for seeing config is being available in deploy script or not js (more block confirmation will take more time)
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+      blockConfirmations: 6,
+    },
   },
   namedAccounts: {
     deployer: {
@@ -24,5 +27,5 @@ module.exports = {
       1: 0,
     },
   },
-  blockConfirmations: 10
+  blockConfirmations: 10,
 };
